fdb6841d329f51875576534f1033d7a173f6a75b
Bruno Paz <184563+brpaz@users.noreply.github.com>
2023-11-03 10:26:08 +0000

    fix: add trust flag when running copier

--- internal/copier/runner.go
+++ internal/copier/runner.go
@@ -7,7 +7,7 @@ import (
 
 // Runcopier-runerator runs the copier copier-runerator with the given git url and destination
 func RunGenerator(gitUrl string, destination string) error {
-	cmd := exec.Command("copier", "copy", gitUrl, destination)
+	cmd := exec.Command("copier", "copy", gitUrl, destination, "--trust")
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
